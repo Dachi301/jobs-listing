384af7402c6e42438e6a2fd06e11698df3f85696
Dachi301 <qirikashvili2020@gmail.com>
2024-02-16 13:08:24 +0400

    feat: ui components splitted in partials

--- helpers.php
+++ helpers.php
@@ -11,3 +11,63 @@ function basePath ($path = '')
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ *
+ * @param string $name
+ * @return string
+ *
+ */
+
+function loadView ($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View '{$name} not found!'";
+    }
+}
+
+/**
+ *
+ * @param string $name
+ * @return void
+ *
+ */
+
+function loadPartial ($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "View '{$name} not found!'";
+    }
+}
+
+/**
+ *
+ * @param mixed $value
+ * @return @void
+ */
+
+function inspect($value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ *
+ * @param mixed $value
+ * @return @void
+ */
+
+function inspectAndDie($value) {
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
+}
